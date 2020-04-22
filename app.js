45dd62142636c1afcb4dde2821f8257270fbaacd
dohkim <dh54kim54@gmail.com>
2020-04-22 12:19:49 +0900

    update feature: response to user

--- app.js
+++ app.js
@@ -6,9 +6,21 @@ const app = new App({
   signingSecret: process.env.SLACK_SIGNING_SECRET
 });
 
+app.command('/somoim', async ({ command, ack, say }) => {
+  // Acknowledge command request
+  await ack();
+
+  if (`${command.text}` === 'register')
+    await say("you called register");
+  else if (`${command.text}` === 'list')
+    await say("you called list");
+  else if (`${command.text}` === 'unregister')
+    await say("you called unregister")
+});
+
 (async () => {
   // Start your app
   await app.start(process.env.PORT || 3000);
 
-  console.log('?? Bolt app is running!');
+  console.log('Bolt app is running!');
 })();
