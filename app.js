fd053c45e37350c2b4a00c19de15e649f887a81f
hjeon <hjeon@student.42seoul.kr>
2020-04-23 10:21:59 +0900

    delete comment

--- app.js
+++ app.js
@@ -1,13 +1,11 @@
 const { App } = require('@slack/bolt');
 
-// Initializes your app with your bot token and signing secret
 const app = new App({
   token: process.env.SLACK_BOT_TOKEN,
   signingSecret: process.env.SLACK_SIGNING_SECRET
 });
 
 app.command('/somoim', async ({ command, ack, say }) => {
-  // Acknowledge command request
   await ack();
 
   if (`${command.text}` === 'register')
@@ -19,8 +17,6 @@ app.command('/somoim', async ({ command, ack, say }) => {
 });
 
 (async () => {
-  // Start your app
   await app.start(process.env.PORT || 3000);
-
   console.log('Bolt app is running!');
 })();
