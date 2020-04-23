fe9532b971af0233ad388c8261749a56c4080273
dohkim <dh54kim54@gmail.com>
2020-04-23 13:31:51 +0900

    modal block

---
+++ src/register.ts
@@ -0,0 +1,62 @@
+// [
+// 	{
+// 		"type": "input",
+// 		"element": {
+// 			"type": "plain_text_input",
+// 			"action_id": "title",
+// 			"placeholder": {
+// 				"type": "plain_text",
+// 				"text": "What's the name of your Somoim?"
+// 			}
+// 		},
+// 		"label": {
+// 			"type": "plain_text",
+// 			"text": "Somoim name"
+// 		}
+// 	},
+// 	{
+// 		"type": "input",
+// 		"element": {
+// 			"type": "plain_text_input",
+// 			"action_id": "option_1",
+// 			"placeholder": {
+// 				"type": "plain_text",
+// 				"text": "Choose the best emoticon for your Somoim"
+// 			}
+// 		},
+// 		"label": {
+// 			"type": "plain_text",
+// 			"text": "Somoim emoji"
+// 		}
+// 	},
+// 	{
+// 		"type": "input",
+// 		"element": {
+// 			"type": "plain_text_input",
+// 			"action_id": "option_2",
+// 			"placeholder": {
+// 				"type": "plain_text",
+// 				"text": "How many options do they need, really?"
+// 			}
+// 		},
+// 		"label": {
+// 			"type": "plain_text",
+// 			"text": "모임을 표현할 수 있는 간단한 설명"
+// 		}
+// 	},
+// 	{
+// 		"type": "input",
+// 		"element": {
+// 			"type": "plain_text_input",
+// 			"action_id": "option_3",
+// 			"placeholder": {
+// 				"type": "plain_text",
+// 				"text": "카톡 오픈채팅, 디스코드"
+// 			}
+// 		},
+// 		"label": {
+// 			"type": "plain_text",
+// 			"text": "모임 url"
+// 		}
+// 	}
+// ]
