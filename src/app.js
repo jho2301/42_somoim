d37c6b35167b2c6b21448af3f3b9a0ed9621eeed
dohkim <dh54kim54@gmail.com>
2020-05-01 19:02:34 +0900

    register finished excluding optional_advertise

--- src/app.js
+++ src/app.js
@@ -1,139 +1,140 @@
 // eslint-disable-next-line import/no-unresolved
-const dotenv = require('dotenv');
-const { App } = require('@slack/bolt');
-const { SomoimDB } = require('./db');
+const dotenv = require("dotenv");
+const { App } = require("@slack/bolt");
+const { SomoimDB } = require("./db");
+const { getUserCampus, Campus } = require("./campus_classification");
 
 dotenv.config();
 
 const showList = [
   {
-    type: 'section',
+    type: "section",
     text: {
-      type: 'mrkdwn',
-      text: '*Which somoim would you like to join?* ',
+      type: "mrkdwn",
+      text: "*Whicbuttonh somoim would you like to join?* ",
     },
   },
   {
-    type: 'divider',
+    type: "divider",
   },
   {
-    type: 'section',
+    type: "section",
     text: {
-      type: 'mrkdwn',
+      type: "mrkdwn",
       text:
-        ':soccer: *42 풋살 동아리*\n 개발(develop아님 ㅎ)도 같이 즐겨요! 매주 일요일 아카데미 축구장에서 풋살 합니다.',
+        ":soccer: *42 풋살 동아리*\n 개발(develop아님 ㅎ)도 같이 즐겨요! 매주 일요일 아카데미 축구장에서 풋살 합니다.",
     },
     accessory: {
-      type: 'button',
+      type: "button",
       text: {
-        type: 'plain_text',
+        type: "plain_text",
         emoji: false,
-        text: 'Join',
+        text: "Join",
       },
-      value: 'open.kakao.com',
+      value: "open.kakao.com",
     },
   },
   {
-    type: 'divider',
+    type: "divider",
   },
   {
-    type: 'section',
+    type: "section",
     text: {
-      type: 'mrkdwn',
+      type: "mrkdwn",
       text:
-        ':deciduous_tree: *모여봐요 모동숲*\n 현생을 피해 섬으로 피신하신 분들은 이쪽으로 오세요 모동숲 얘기만 해요!',
+        ":deciduous_tree: *모여봐요 모동숲*\n 현생을 피해 섬으로 피신하신 분들은 이쪽으로 오세요 모동숲 얘기만 해요!",
     },
     accessory: {
-      type: 'button',
+      type: "button",
       text: {
-        type: 'plain_text',
+        type: "plain_text",
         emoji: false,
-        text: 'Join',
+        text: "Join",
       },
-      value: 'open.kakao.com',
+      value: "open.kakao.com",
     },
   },
   {
-    type: 'divider',
+    type: "divider",
   },
   {
-    type: 'section',
+    type: "section",
     text: {
-      type: 'mrkdwn',
+      type: "mrkdwn",
       text:
-        ':book: *프로그래밍 책 같이 읽기*\n 혼자 하면 힘들더라구요... 프로그래밍 관련 서적들 같이 읽고 의견 나눠봐요.',
+        ":book: *프로그래밍 책 같이 읽기*\n 혼자 하면 힘들더라구요... 프로그래밍 관련 서적들 같이 읽고 의견 나눠봐요.",
     },
     accessory: {
-      type: 'button',
+      type: "button",
       text: {
-        type: 'plain_text',
+        type: "plain_text",
         emoji: false,
-        text: 'Join',
+        text: "Join",
       },
-      value: 'open.kakao.com',
+      value: "open.kakao.com",
     },
   },
   {
-    type: 'divider',
+    type: "divider",
   },
   {
-    type: 'section',
+    type: "section",
     text: {
-      type: 'mrkdwn',
-      text: ':video_game: *42롤*\n 롤 같이 하실 분들. 브실골 플다챌 모두 오세요ㅎㅎ',
+      type: "mrkdwn",
+      text: ":video_game: *42롤*\n 롤 같이 하실 분들. 브실골 플다챌 모두 오세요ㅎㅎ",
     },
     accessory: {
-      type: 'button',
+      type: "button",
       text: {
-        type: 'plain_text',
+        type: "plain_text",
         emoji: false,
-        text: 'Join',
+        text: "Join",
       },
-      value: 'open.kakao.com',
+      value: "open.kakao.com",
     },
   },
   {
-    type: 'divider',
+    type: "divider",
   },
   {
-    type: 'section',
+    type: "section",
     text: {
-      type: 'mrkdwn',
-      text: ':musical_note: *노동요 같이 들어요*\n 코딩할 때 듣는 음악 추천하는 방',
+      type: "mrkdwn",
+      text: ":musical_note: *노동요 같이 들어요*\n 코딩할 때 듣는 음악 추천하는 방",
     },
     accessory: {
-      type: 'button',
+      type: "button",
       text: {
-        type: 'plain_text',
+        type: "plain_text",
         emoji: false,
-        text: 'Join',
+        text: "Join",
       },
-      value: 'open.kakao.com',
+      value: "open.kakao.com",
     },
   },
   {
-    type: 'divider',
+    type: "divider",
   },
   {
-    type: 'actions',
+    type: "actions",
     elements: [
       {
-        type: 'button',
+        type: "button",
         text: {
-          type: 'plain_text',
+          type: "plain_text",
           emoji: true,
-          text: 'Previous',
+          text: "Previous",
         },
-        value: 'go_to_previous_value',
+        value: "go_to_previous_value",
       },
       {
-        type: 'button',
+        type: "button",
         text: {
-          type: 'plain_text',
+          type: "plain_text",
           emoji: true,
-          text: 'Next',
+          text: "Next",
         },
-        value: 'go_to_next_value',
+        value: "go_to_next_value",
       },
     ],
   },
@@ -144,175 +145,216 @@ const app = new App({
   signingSecret: process.env.SLACK_SIGNING_SECRET,
 });
 
-async function viewModal(body, context, client) {
+async function register(body, context, client) {
   try {
-    const result = await client.views.open({
+      await client.views.open({
       token: context.botToken,
       trigger_id: body.trigger_id,
       view: {
-        type: 'modal',
+        type: "modal",
+        callback_id: "register-view",
         title: {
-          type: 'plain_text',
-          text: 'Somoim register',
+          type: "plain_text",
+          text: "Register Somoim",
           emoji: true,
         },
         submit: {
-          type: 'plain_text',
-          text: 'Submit',
+          type: "plain_text",
+          text: "Submit",
           emoji: true,
         },
         close: {
-          type: 'plain_text',
-          text: 'Cancel',
+          type: "plain_text",
+          text: "Cancel",
           emoji: true,
         },
         blocks: [
           {
-            type: 'section',
+            type: "section",
             text: {
-              type: 'plain_text',
-              text: ':wave: Hello!\n\nPlease register your Somoim',
+              type: "plain_text",
+              text: ":wave: Hello!\n\nPlease register your Somoim",
               emoji: true,
             },
           },
           {
-            type: 'divider',
+            type: "divider",
           },
           {
-            type: 'input',
+            type: "input",
             element: {
-              type: 'plain_text_input',
-              action_id: 'option_0',
+              type: "plain_text_input",
+              action_id: "somoim_name",
               placeholder: {
-                type: 'plain_text',
-                text: 'Name of your Somoim',
+                type: "plain_text",
+                text: "Name of your Somoim",
               },
             },
             label: {
-              type: 'plain_text',
+              type: "plain_text",
               text: "What's the name of your Somoim?",
               emoji: true,
             },
           },
           {
-            type: 'input',
+            type: "input",
             element: {
-              type: 'plain_text_input',
-              action_id: 'option_1',
+              type: "plain_text_input",
+              action_id: "represent_emoji",
               placeholder: {
-                type: 'plain_text',
-                text: 'Choose the best emoji for your Somoim',
+                type: "plain_text",
+                text: "Choose the best emoji for your Somoim",
               },
             },
             label: {
-              type: 'plain_text',
-              text: 'Best emoji for your Somoim',
+              type: "plain_text",
+              text: "Best emoji for your Somoim",
               emoji: true,
             },
           },
           {
-            type: 'input',
+            type: "input",
             element: {
-              type: 'plain_text_input',
-              action_id: 'option_1',
+              type: "plain_text_input",
+              action_id: "description",
               placeholder: {
-                type: 'plain_text',
-                text: '소모임 이름을 적어주세요',
+                type: "plain_text",
+                text: "brief introduce",
               },
             },
             label: {
-              type: 'plain_text',
-              text: "What's the name of your Somoim?",
+              type: "plain_text",
+              text: "Brief introduce",
               emoji: true,
+            },
+          },
+          {
+            type: "input",
+            element: {
+              type: "plain_text_input",
+              action_id: "somoim_url",
+              placeholder: {
+                type: "plain_text",
+                text: "URL",
+              },
+            },
+            label: {
+              type: "plain_text",
+              text: "Discord/Kakao talk link",
+              emoji: true,
+            },
+          },
+          {
+            type: "section",
+            text: {
+              type: "mrkdwn",
+              text: "Select to advertise (optional)",
+            },
+            accessory: {
+              type: "checkboxes",
+              options: [
+                {
+                  text: {
+                    type: "plain_text",
+                    text: "advertise to random channel",
+                    emoji: true,
+                  },
+                  description: {
+                    type: "plain_text",
+                    text: "to your own campus random channel",
+                    emoji: true,
+                  },
+                  value: "advertise",
+                },
+              ],
             },
           },
         ],
       },
     });
-    console.log(result);
   } catch (error) {
     console.error(error);
   }
 }
 
-async function unregisterModal(body, context, client) {
+async function unregister(body, context, client) {
   try {
     const result = await client.views.open({
       token: context.botToken,
       trigger_id: body.trigger_id,
       view: {
-        type: 'modal',
+        type: "modal",
         title: {
-          type: 'plain_text',
-          text: 'Somoim register',
+          type: "plain_text",
+          text: "Somoim register",
           emoji: true,
         },
         submit: {
-          type: 'plain_text',
-          text: 'Submit',
+          type: "plain_text",
+          text: "Submit",
           emoji: true,
         },
         close: {
-          type: 'plain_text',
-          text: 'Cancel',
+          type: "plain_text",
+          text: "Cancel",
           emoji: true,
         },
         blocks: [
           {
-            type: 'section',
+            type: "section",
             text: {
-              type: 'mrkdwn',
-              text: 'Hello 👋\n\n Unregister your somoim',
+              type: "mrkdwn",
+              text: "Hello 👋\n\n Unregister your somoim",
             },
           },
           {
-            type: 'divider',
+            type: "divider",
           },
           {
-            type: 'section',
+            type: "section",
             text: {
-              type: 'mrkdwn',
-              text: '삭제할 소모임을 골라주세요',
+              type: "mrkdwn",
+              text: "삭제할 소모임을 골라주세요",
             },
             accessory: {
-              type: 'static_select',
+              type: "static_select",
               placeholder: {
-                type: 'plain_text',
-                text: '소모임 고르기',
+                type: "plain_text",
+                text: "소모임 고르기",
                 emoji: true,
               },
               options: [
                 {
                   text: {
-                    type: 'plain_text',
-                    text: ':deciduous_tree: 모여봐요 모동숲',
+                    type: "plain_text",
+                    text: ":deciduous_tree: 모여봐요 모동숲",
                     emoji: true,
                   },
-                  value: 'value-0',
+                  value: "value-0",
                 },
                 {
                   text: {
-                    type: 'plain_text',
-                    text: ':soccer: 42 풋살 동아리',
+                    type: "plain_text",
+                    text: ":soccer: 42 풋살 동아리",
                     emoji: true,
                   },
-                  value: 'value-1',
+                  value: "value-1",
                 },
                 {
                   text: {
-                    type: 'plain_text',
-                    text: ':musical_note: 노동요 같이 들어요',
+                    type: "plain_text",
+                    text: ":musical_note: 노동요 같이 들어요",
                     emoji: true,
                   },
-                  value: 'value-2',
+                  value: "value-2",
                 },
                 {
                   text: {
-                    type: 'plain_text',
-                    text: ':video_game: 42롤',
+                    type: "plain_text",
+                    text: ":video_game: 42롤",
                     emoji: true,
                   },
-                  value: 'value-3',
+                  value: "value-3",
                 },
               ],
             },
@@ -326,28 +368,112 @@ async function unregisterModal(body, context, client) {
   }
 }
 
-app.command('/somoim', async ({ command, ack, body, context, client }) => {
+app.command("/somoim", async ({ command, ack, body, context, client }) => {
   await ack();
+  // const userinfo = await app.client.users.info({
+  //   token: process.env.SLACK_BOT_TOKEN,
+  //   user: command.user_id,
+  // });
 
-  const userinfo = await app.client.users.info({
-    token: process.env.SLACK_BOT_TOKEN,
-    user: command.user_id,
-  });
-  console.log(userinfo);
-
-  if (`${command.text}` === 'register') await viewModal(body, context, client);
-  else if (`${command.text}` === 'list')
+  if (`${command.text}` === "register") await register(body, context, client);
+  else if (`${command.text}` === "list")
     await app.client.chat.postEphemeral({
       token: process.env.SLACK_BOT_TOKEN,
       channel: command.channel_id,
       user: command.user_id,
       blocks: showList,
-      text: 'you called somoim list',
+      text: "you called somoim list",
     });
-  else if (`${command.text}` === 'unregister') await unregisterModal(body, context, client);
+  else if (`${command.text}` === "unregister") await unregister(body, context, client);
 });
+
+app.view("register-view", async ({ ack, body, view, context, client }) => {
+  await ack();
+
+  const userinfo = await app.client.users.info({
+    token: process.env.SLACK_BOT_TOKEN,
+    user: body.user.id,
+  });
+  const campusName = await getUserCampus(userinfo.user.profile.email);
+
+  let blockId = view.blocks[2].block_id;
+  const somoimName = view.state.values[blockId].somoim_name.value;
+  blockId = view.blocks[3].block_id;
+  const emoji = view.state.values[blockId].represent_emoji.value;
+  blockId = view.blocks[4].block_id;
+  const desc = view.state.values[blockId].description.value;
+  blockId = view.blocks[5].block_id;
+  const url = view.state.values[blockId].somoim_url.value;
+
+    await SomoimDB.create({
+      campus: campusName,
+      somoim_name: somoimName,
+      represent_emoji: emoji,
+      description: desc,
+      somoim_url: url,
+      registant_name: body.user.name
+    }).then((somoim) => {
+      console.log("data created!! id:", somoim.id);
+      client.views.open({
+        token: context.botToken,
+        trigger_id: body.trigger_id,
+        view:
+      {
+        "type": "modal",
+        "title": {
+          "type": "plain_text",
+          "text": "create success",
+          "emoji": true
+        },
+        "close": {
+          "type": "plain_text",
+          "text": "Close",
+          "emoji": true
+        },
+        "blocks": [
+          {
+            "type": "section",
+            "text": {
+              "type": "mrkdwn",
+              "text": "\n성공했어~"
+            }
+          }
+        ]
+      }})
+    }
+    ).catch(err => {
+      console.log('failed to create\n', err);
+      client.views.open({
+        token: context.botToken,
+        trigger_id: body.trigger_id,
+        view:
+      {
+        "type": "modal",
+        "title": {
+          "type": "plain_text",
+    	"text": "Error Occured",
+          "emoji": true
+        },
+        "close": {
+          "type": "plain_text",
+          "text": "Close",
+          "emoji": true
+        },
+        "blocks": [
+          {
+            "type": "section",
+            "text": {
+              "type": "mrkdwn",
+              "text": "\n이름이 중복됐어요~"
+            }
+          }
+        ]
+      }})
+    });
+  }
+);
 
 (async () => {
   await app.start(process.env.PORT || 3000);
-  console.log('Bolt app is running!');
+  console.log("Bolt app is running!");
 })();
