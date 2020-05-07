add453e763bd6ed85bbf862f6731befd243ac65b
hjeon <hjeon@student.42seoul.kr>
2020-05-07 16:05:31 +0900

    add environment variable: DB_DIALECT

--- src/model.js
+++ src/model.js
@@ -9,7 +9,7 @@ const sequelize = new Sequelize(
   process.env.DB_PASSWORD || '',
   {
     host: process.env.DB_HOST || 'localhost',
-    dialect: 'mysql',
+    dialect: process.env.DB_DIALECT || 'mysql',
     dialectOptions: {
       charset: 'utf8mb4',
     },
